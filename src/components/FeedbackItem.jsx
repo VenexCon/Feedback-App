b7df735dafebcf1f7cf50882048d007eaa204199
VenexCon <venexconsultant@outlook.com>
2022-09-28 20:36:21 +0100

    <feat> included stats banner and feedbackStats.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,16 @@
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card" //imported styles
 
-function FeedbackItem({item}) { //Each feedback card, receives state from data folder 
+function FeedbackItem({item, handleDelete}) { //Each feedback card, receives state from data folder 
+ 
 
   return (
     <Card>
         <div className="num-display">{item.rating}</div> 
+        <button className="close">
+          <FaTimes onClick={() => handleDelete(item.id)} color = 'purple' />
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({feedback}) { //App passes in feedback data
+function FeedbackList({feedback, handleDelete}) { //App passes in feedback data
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback yet</p>
     }
@@ -9,7 +9,10 @@ function FeedbackList({feedback}) { //App passes in feedback data
   return (
     <div className="feedback-list">
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item = {item} />
+            <FeedbackItem 
+            key={item.id} 
+            item = {item} 
+            handleDelete ={handleDelete}  />
         ))}
     </div>
   )
